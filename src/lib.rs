5269a57d908a37d2ae61047a7729206f7ec24c68
Nguyen Duong <muoi07052001@gmail.com>
2022-08-04 21:27:43 +0700

    Add update + delete func

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, AccountId, Balance, PanicOnDefault, Promise, Timestamp};
+use near_sdk::{env, near_bindgen, AccountId, Balance, PanicOnDefault, Promise, Timestamp, log};
 
 pub type UserId = u32;
 pub type CriteriaId = u32;
